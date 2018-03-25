8045c75b3b8a954e1828e257d5c7af464aa69a53
NYDanielLim <lim.daniel.10@gmail.com>
2018-03-25 01:01:37 -0400

    Daniel's Edits
    -Radio Buttons
    -Suppressions
    -User Control additions so we can click top buttons

---
+++ DHCPControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EZNIC
+{
+    public partial class DHCPControl : UserControl
+    {
+        public DHCPControl()
+        {
+            InitializeComponent();
+        }
+
+#pragma warning disable IDE1006 // Naming Styles
+        private void label1_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006 // Naming Styles
+        {
+
+        }
+    }
+}
